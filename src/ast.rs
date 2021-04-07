9e1be53d79afe05190f4136ad897e6d6829349b6
multicatch <cezaryre@gmail.com>
2021-04-07 18:52:55 +0200

    Rewritten AST parsing

--- src/ast.rs
+++ src/ast.rs
@@ -1,58 +1,86 @@
 #[cfg(test)]
 mod tests {
     use crate::ast::{parse_ast, SyntaxNode};
+    use crate::ast::SyntaxNode::{AnonymousNode, NamedNode};
 
     #[test]
-    fn it_works() {
+    fn should_parse_ast() {
         let input = "{{ (list 1 2 (if a b c)) }}".to_string();
-        let node = parse_ast(input);
-        let expected = "SyntaxNode { identifier: None, children: [SyntaxNode { identifier: Some(\"list\"), children: [SyntaxNode { identifier: Some(\"1\"), children: [], position: 6, end_position: 7 }, SyntaxNode { identifier: Some(\"2\"), children: [], position: 8, end_position: 9 }, SyntaxNode { identifier: Some(\"if\"), children: [SyntaxNode { identifier: Some(\"a\"), children: [], position: 13, end_position: 14 }, SyntaxNode { identifier: Some(\"b\"), children: [], position: 15, end_position: 16 }, SyntaxNode { identifier: Some(\"c\"), children: [], position: 17, end_position: 18 }], position: 10, end_position: 18 }], position: 1, end_position: 19 }], position: 0, end_position: 20 }".to_string();
-        assert_eq!(expected, format!("{:?}", &node));
+        let actual = parse_ast(input);
+
+        let expected = AnonymousNode {
+            children: vec![
+                NamedNode {
+                    identifier: "list".to_string(),
+                    children: vec![
+                        NamedNode {
+                            identifier: "1".to_string(),
+                            children: vec![],
+                        },
+                        NamedNode {
+                            identifier: "2".to_string(),
+                            children: vec![],
+                        },
+                        NamedNode {
+                            identifier: "if".to_string(),
+                            children: vec![
+                                NamedNode {
+                                    identifier: "a".to_string(),
+                                    children: vec![],
+                                },
+                                NamedNode {
+                                    identifier: "b".to_string(),
+                                    children: vec![],
+                                },
+                                NamedNode {
+                                    identifier: "c".to_string(),
+                                    children: vec![],
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ]
+        };
+        assert_eq!(expected, actual);
     }
 }
 
 use crate::parser::{START_PATTERN, END_PATTERN};
 use std::fmt::Debug;
+use std::process::id;
 
-#[derive(Debug)]
-pub struct SyntaxNode {
-    identifier: Option<String>,
-    children: Vec<SyntaxNode>,
-    position: usize,
-    end_position: usize,
+/// Represents a node in an AST
+#[derive(Clone, Debug, PartialEq)]
+pub enum SyntaxNode {
+    NamedNode {
+        identifier: String,
+        children: Vec<SyntaxNode>,
+    },
+    AnonymousNode {
+        children: Vec<SyntaxNode>
+    },
 }
 
-impl Clone for SyntaxNode {
-    fn clone(&self) -> Self {
-        SyntaxNode {
-            identifier: self.identifier.clone(),
-            children: self.children.clone(),
-            position: self.position.clone(),
-            end_position: self.position.clone(),
-        }
-    }
-
-    fn clone_from(&mut self, source: &Self) {
-        self.identifier = source.identifier.clone();
-        self.children = source.children.clone();
-        self.position = source.position.clone();
-        self.end_position = source.end_position.clone();
-    }
-}
-
+/// Used for parsing AST for further evaluation.
+///
+/// This function tries to parse AST assuming it is Lisp-like syntax, which is practically
+/// `function arg0 arg1 arg2`, where `function` is the function name, and `arg0...` are the arguments.
+///
+/// It also allows to use parenthesis to evaluate a nested function.
+///
 pub fn parse_ast(source: String) -> SyntaxNode {
     let source = source.strip_prefix(START_PATTERN).unwrap_or(source.as_str());
     let source = source.strip_suffix(END_PATTERN).unwrap_or(source);
 
-    next_node_of(source, 0)
+    next_node_of(source, 0).0
 }
 
-fn next_node_of(source: &str, offset: usize) -> SyntaxNode {
-    let mut syntax_node = SyntaxNode {
-        identifier: None,
+struct SyntaxScanResult(SyntaxNode, usize);
+
+fn next_node_of(source: &str, offset: usize) -> SyntaxScanResult {
+    let mut syntax_node = SyntaxNode::AnonymousNode {
         children: vec![],
-        position: offset,
-        end_position: offset,
     };
     let mut identifier = "".to_string();
     let mut skip_end: usize = 0;
@@ -64,48 +92,63 @@ fn next_node_of(source: &str, offset: usize) -> SyntaxNode {
         }
         let current_offset = index + 1;
         let source_remainder = &source[current_offset..];
+
         if char == '(' {
-            syntax_node.add_child_or_update(identifier.clone(), position);
-            let child = next_node_of(source_remainder, position);
-            skip_end = child.end_position.clone();
-            syntax_node.children.push(child);
+            if let Some(mut node) = syntax_node.add_identifier_or_child(&identifier) {
+                syntax_node = node;
+            }
+            let SyntaxScanResult(child, skip_pos) = next_node_of(source_remainder, position);
+            skip_end = skip_pos;
+            syntax_node.add_child(child);
         } else {
             if char == ' ' || char == ')' {
-                syntax_node.add_child_or_update(identifier.clone(), position);
+                if let Some(node) = syntax_node.add_identifier_or_child(&identifier) {
+                    syntax_node = node;
+                }
                 identifier.clear();
             } else {
                 identifier.push(char)
             }
 
             if char == ')' {
-                syntax_node.end_position = position;
-                let result = syntax_node;
-                return result;
+                return SyntaxScanResult(syntax_node, position);
             }
         }
     }
-    syntax_node.end_position = offset + source.len();
-    syntax_node.clone()
+
+    SyntaxScanResult(syntax_node, offset + source.len())
 }
 
 impl SyntaxNode {
     fn add_child(&mut self, child: SyntaxNode) {
-        self.children.push(child);
+        match self {
+            SyntaxNode::AnonymousNode { children } => children.push(child),
+            SyntaxNode::NamedNode { children, .. } => children.push(child),
+        }
     }
 
-    fn add_child_or_update(&mut self, identifier: String, position: usize) {
-        if !identifier.is_empty() {
-            let syntax_node_id = self.identifier.clone();
-            if syntax_node_id == None {
-                self.identifier = Some(identifier.clone());
-            } else {
-                let node = SyntaxNode {
-                    identifier: Some(identifier.clone()),
+    fn add_identifier_or_child(&self, new_identifier: &String) -> Option<SyntaxNode> {
+        if new_identifier.is_empty() {
+            return None;
+        }
+
+        match self {
+            SyntaxNode::AnonymousNode { children } =>
+                Some(SyntaxNode::NamedNode {
+                    identifier: new_identifier.clone(),
+                    children: children.clone(),
+                }),
+
+            SyntaxNode::NamedNode { identifier, children } => {
+                let mut children = children.clone();
+                children.push(SyntaxNode::NamedNode {
+                    identifier: new_identifier.clone(),
                     children: vec![],
-                    position: position - identifier.len(),
-                    end_position: position,
-                };
-                self.add_child(node);
+                });
+                Some(SyntaxNode::NamedNode {
+                    identifier: identifier.clone(),
+                    children,
+                })
             }
         }
     }
