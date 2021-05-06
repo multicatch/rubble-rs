3e56eac64e4052e5124bce91d0a2f24812471e71
multicatch <cezaryre@gmail.com>
2021-05-06 06:46:49 +0000

    Deploying to gh-pages from @ multicatch/rubble-rs@ba8b4edc61f6ba45421391fd946076625c2d7280 🚀

---
+++ rubble_templates/functions/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["concat_function","Concatenates the arguments."],["std_functions","Provides a set of standard functions."]]});
--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["rubble_templates"] = {"name":"","dirs":[{"name":"evaluator","files":["ast.rs","engine.rs"]},{"name":"template","files":["content.rs"]}],"files":["compiler.rs","evaluator.rs","lib.rs","template.rs"]};
+sourcesIndex["rubble_templates"] = {"name":"","dirs":[{"name":"evaluator","files":["ast.rs","engine.rs"]},{"name":"template","files":["content.rs"]}],"files":["compiler.rs","evaluator.rs","functions.rs","lib.rs","template.rs"]};
 createSourceSidebar();
